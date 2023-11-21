eeddf0dbaf6b99028df1081e2a854bdd1387acec
Eliel Moura <eliellpf@gmail.com>
2023-11-21 10:52:27 -0300

    correção do exercicio

--- 02-funcoes.js
+++ 02-funcoes.js
@@ -11,12 +11,44 @@ const exemplo01 = function(){
 
 exemplo01();
 
-/* Sintaxe declarada/nomeada
-
-Obs: nesta sintaxe, você pode chamar a função antes ou depois de declará-la. Portanto, a ordem não importa
-*/
+// Sintaxe declarada/nomeada
+// Obs.: Nesta sintaxe, voc~e pode chamar a função antes ou depois de declará-la. Portanto, a ordem não importa.
 function exemplo02(){
     console.log("Função nomeada!");
 };
 
 exemplo02();
+
+// Correção
+/* Exercício
+1) Crie uma função (qualquer sintaxe) que receba dois valores numéricos, calcule a diferença entre eles e retorne o resultado.
+*/
+function exercicio01(valor1, valor2){
+    //1° Versão: Variável de escopo LOCAL
+    // let resultado = valor1 - valor2;
+    // return resultado;
+
+    //2° Versão
+    return valor1 - valor2;
+}
+
+/*
+2) Chame esta função duas vezes passando valores diferentes e guardando-os em variáveis de escopo global.
+*/
+let resultado1 = exercicio01(10, 5);
+let resultado2 = exercicio01(120, 66)
+/*
+3) Mostre os valores processados pela função no terminal.
+*/
+console.log(resultado1);
+console.log(resultado2);
+
+// Meu exercício
+// function exercicio01(){
+//     var resultado = valor1 - valor2;
+//     return resultado;
+// }
+
+// console.log(exercicio01(valor1 = 100, valor2 = 30));
+
+// console.log(exercicio01(valor1 = 1000, valor2 = 300));
