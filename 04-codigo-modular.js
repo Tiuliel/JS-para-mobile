9d03a31f26aad28e2c6456bdd67fd5bcd8d2f5fa
Eliel Moura <eliellpf@gmail.com>
2023-11-22 11:31:13 -0300

    teste de módulos no HTML

--- 04-codigo-modular.js
+++ 04-codigo-modular.js
@@ -1,7 +1,11 @@
 /* Podemos usar "alias/apelidos" com o comando "as" para evitar conflitos entre módulos de mesmo nome */
 import {pessoa, livro, alunos as reprovados} from "./modulos/dados.js";
 import alunos from "./modulos/alunos.js";
-import { converteMaiusculas, converteMinusculas, formataMoeda } from "./modulos/funcoes.js";
+//importação manual de cada função
+//import { converteMaiusculas, converteMinusculas, formataMoeda } from "./modulos/funcoes.js";
+
+/* Importação das funções através de um apelido/alias */
+import * as minhasFuncoes from "./modulos/funcoes.js";
 
 
 
@@ -22,13 +26,13 @@ for (  const prop in pessoa) {
 console.log(livro.titulo);
 console.log(livro.volume);
 
-console.log(converteMaiusculas(reprovados[0]));
-console.log(converteMinusculas(alunos[0]));
+console.log(minhasFuncoes.converteMaiusculas(reprovados[0]));
+console.log(minhasFuncoes.converteMinusculas(alunos[0]));
 
 let preco1 = 1200.88
 let preco2 = 1000000.75
 let preco3 = 500.8452
 
-console.log(formataMoeda(preco1));
-console.log(formataMoeda(preco2));
-console.log(formataMoeda(preco3));
+console.log(minhasFuncoes.formataMoeda(preco1));
+console.log(minhasFuncoes.formataMoeda(preco2));
+console.log(minhasFuncoes.formataMoeda(preco3));
