1aadca73742ed0ae2433dc7f7accd03f088a9867
Eliel Moura <eliellpf@gmail.com>
2023-11-22 11:43:59 -0300

    spread com array

---
+++ 05-operadores-spread.js
@@ -0,0 +1,10 @@
+   /*  ...spread -> espalhar
+   copiar elementos/dados de um array/objeto
+   para dentro de outro array/objeto */
+
+   const bandas = ["Pink Floyd", "Rush", "Yes"];
+   const maisBandas = [...bandas, "Slayer", "Nightwish"];
+
+   console.log(bandas); 
+   console.log(maisBandas); 
+  
