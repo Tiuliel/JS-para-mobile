431b5366f20e18ccc1abe9b01b9bb7accbd22ea5
Eliel Moura <eliellpf@gmail.com>
2023-11-22 11:52:41 -0300

    spread

--- 05-operadores-spread.js
+++ 05-operadores-spread.js
@@ -2,9 +2,28 @@
    copiar elementos/dados de um array/objeto
    para dentro de outro array/objeto */
 
+
+   // Spread com arrays
    const bandas = ["Pink Floyd", "Rush", "Yes"];
    const maisBandas = [...bandas, "Slayer", "Nightwish"];
 
    console.log(bandas); 
    console.log(maisBandas); 
   
+
+   //Spread com objetos
+const cliente = {
+    nome: "Mocoto",
+    cidade: "São Paulo",
+    idade: 55
+}
+
+const novosDados = {
+    ...cliente, // Copiando as props. do cliente pra cá
+    estado: "SP",
+    pedido: "123xyz",
+    total: 5000.88,
+    idade: 60 // subreescrevendo valores de propriedades
+};
+
+console.log(novosDados);
